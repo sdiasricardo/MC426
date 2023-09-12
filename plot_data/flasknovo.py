5157ae686dc10dee7ac9707db04e061d03713df5
Rafael Gregori <rafaelgregori1337@gmail.com>
2023-09-12 10:58:21 -0300

    implemented JSON parsing

--- plot_data/flasknovo.py
+++ plot_data/flasknovo.py
@@ -3,21 +3,27 @@ import pandas as pd
 import json
 import numpy as np
 
-data = json.load(open('forecastCampinas.json'))
-
-df = pd.DataFrame(data['list'])
-print(df)
-
-
-df2 = pd.DataFrame({'dt': df['dt']})
-df2['temp'] = np.zeros(df.shape[0])
-for i in range(df2.shape[0]):
-    df2['temp'][i] = (df['main'][i]['temp'])
-print(df2)
 
 
 
-def create_plot(selected_column: str, selected_plot_type: str):
+
+def parseColumnJSON(json_file, column: str):
+    '''
+    Retorna um dataframe com colunas dt e column
+    '''
+    data = json.load(open(json_file))
+    df = pd.DataFrame(data['list'])
+
+    df2 = pd.DataFrame({'dt': df['dt']})
+
+    df2[column] = np.zeros(df.shape[0])
+    for i in range(df2.shape[0]):
+        df2[column][i] = (df['main'][i][column])
+
+    return df2
+
+def create_plot(json_file, selected_column: str, selected_plot_type: str):
+    df2 = parseColumnJSON(json_file, selected_column)
     match selected_plot_type:
         case "histogram":
             fig = px.histogram(df2, x='dt', y=df2[selected_column], title=f'Plot of {selected_column}')
@@ -31,5 +37,5 @@ def create_plot(selected_column: str, selected_plot_type: str):
     return fig
 
 if __name__ == '__main__':
-    fig = create_plot('temp', 'line')
+    fig = create_plot('forecastCampinas.json','temp', 'scatter')
     fig.show()
