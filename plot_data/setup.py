e76165e138cea2870dc92b4f8b63099b490ae33c
ra247346 <ra247346@ad.lab.ic.unicamp.br>
2023-09-14 09:36:40 -0300

    insert setup.py

---
+++ plot_data/setup.py
@@ -0,0 +1,10 @@
+#from setuptools import setup
+
+#setup(
+#    name="my_project",
+#    version="0.1",
+#    packages=["my_package"],
+#    install_requires=[
+#        # List your project's dependencies here
+#    ],
+#)
