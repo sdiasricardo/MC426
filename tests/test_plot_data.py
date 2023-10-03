3727b5f1d0bdb12110c9fce7890d7946eaeac864
Rafael Gregori <rafaelgregori1337@gmail.com>
2023-10-03 10:43:35 -0300

    refactor repository

--- tests/test_plot_data.py
+++ tests/test_plot_data.py
@@ -1,7 +1,17 @@
 import unittest
 import json
+import sys
+import os
+
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+parent_directory = os.path.dirname(current_directory)
+sys.path.append(parent_directory)
+sys.path.append(parent_directory + "/plot_data")
+
 from plot_data import parseMainForecastJSON
 from plot_data import create_plot
+
 
 class TestStringMethods(unittest.TestCase):
 
