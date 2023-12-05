d9e62a086897ffe222781936acae0dfa83d81db7
Rafael Gregori <rafaelgregori1337@gmail.com>
2023-12-05 09:51:58 -0300

    initial adapter implementation

--- ExternalConnections/api/apiHandler.py
+++ ExternalConnections/api/apiHandler.py
@@ -1,15 +1,24 @@
 import requests
-import os
+import os, sys
 #TODO: Colocar dotenv como dependencia do projeto
 from dotenv import load_dotenv
 import json
+import pandas as pd
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+parent_directory = os.path.dirname(current_directory)
+absolute_directory = os.path.dirname(parent_directory)
+sys.path.append(absolute_directory)
+sys.path.append(absolute_directory + "/Services/Dataplot/")
+
+from DataInterface import DataInterface
 
 load_dotenv()
 api_key = os.getenv("weather_api_key")
 
-class ApiHandler:
-    def __init__(self) -> None:
-        pass
+class ApiHandler(DataInterface):
+    def __init__(self):
+        super.__init__()
 
     def queryCityWeather(self, query_type: str, query_place):
         '''
@@ -22,6 +31,18 @@ class ApiHandler:
         base_url = f'http://api.weatherapi.com/v1/{query_type}.json?key={api_key}&q={query_place}&days=7&aqi=no&alerts=yes'
         response = requests.get(base_url)
         if response.status_code == 200:
-            return response.json()
+            self.data = response.json()
         else:
             raise Exception(f'Weather API responded with code {response.status_code}')
+        
+    def get_alerts(self):
+        return self.data['alerts']
+    
+    def get_week_temp(self):
+        days = pd.Series()
+        temp_c, temp_f = pd.Series(), pd.Series()
+        for day in self.data['forecast']['forecastday']:
+        
+
+if __name__ == '__main__':
+    teste = ApiHandler()
