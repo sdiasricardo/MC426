b284809db7004cdb9290c98f33a73a3abb463804
Rafael Gregori <rafaelgregori1337@gmail.com>
2023-12-07 18:35:24 -0300

    adapter implemented

--- ExternalConnections/api/apiHandler.py
+++ ExternalConnections/api/apiHandler.py
@@ -11,6 +11,8 @@ absolute_directory = os.path.dirname(parent_directory)
 sys.path.append(absolute_directory)
 sys.path.append(absolute_directory + "/Services/Dataplot/")
 
+#from DataHandler import DataHandler
+
 load_dotenv()
 api_key = os.getenv("weather_api_key")
 
@@ -22,25 +24,26 @@ class ApiHandler():
             json.dump(data, f)
 
 
-    def queryCityWeather(self, query_type: str, query_place: str):
+    def getJSON(self, query_place: str):
         '''
         Parameters:
-        query_type: current or forecast
         query_place: string representing a city or a tuple [latitude, longitude]
         Stores the api response at DataCache folder
         '''
         if isinstance(query_place, tuple):
             query_place = str(query_place[0]) + ',' + str(query_place[1])
-        base_url = f'http://api.weatherapi.com/v1/{query_type}.json?key={api_key}&q={query_place}&days=7&aqi=no&alerts=yes'
+        base_url = f'http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={query_place}&days=7&aqi=no&alerts=yes'
         response = requests.get(base_url)
         if response.status_code == 200:
             response = response.json()
-            self.write_cache(query_place + '_' + query_type, response)
+            self.write_cache(query_place, response)
             return response
+        elif response.status_code == 400:
+            raise Exception('City not found')
         else:
-            raise Exception(f'Weather API responded with code {response.status_code}')
+            raise Exception(f'Weather responded with code = {response.status_code}')
         
 
 if __name__ == '__main__':
     teste = ApiHandler()
-    print(teste.queryCityWeather('current', 'London'))
+    print(teste.getJSON('Moscow'))
